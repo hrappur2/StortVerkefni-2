98ee437e588bf8fdb9e7cdb65843375783d6aed9
nachos5 <gnorland93@gmail.com>
2017-11-17 03:42:05 +0000

    smá js vinna

--- script.js
+++ script.js
@@ -3,41 +3,47 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 var program = (function() {
-  //skilgreinum breytur hér vegna scope
-  var results;
-  var button;
+  var content;
 
   function click(e) {
     e.preventDefault(); //slökkt á hefðbundinni virkni
     randomString();
   }
 
-  function readJSON(number) {
-    //tímabundið til að vinna locally, þarf að downloada cors ext fyrir chrome svo virki (security shit):
-    var url = 'https://notendur.hi.is/gon2/vefforritun/st%C3%B3rtverkefni2/videos.json';
+  function showData(data) { //sækir gögn og býr til element
+    const lengd = data.categories.length; //hversu margir flokkar
 
+    for (var i=0; i<lengd; i++) { //búum til element fyrir alla flokka
+      createElement(data, i);
+    }
+  }
+
+  function createElement(data, index) {
+    const a = document.createElement('div');
+    a.classList.add('videolist__category');
+    a.innerHTML = data.categories[index].title;
+
+    content.appendChild(a);
+  }
+
+  function readJSON(data) { //fall til að lesa videos.json
+    var url = 'videos.json'; //verður að keyra live server svo virki!
+    var kappa = 'results.videos[0]';
     //showLoading();
-
     var r = new XMLHttpRequest();
     r.open('GET', url, true);
     r.onload = function() {
       if (r.status >= 200 && r.status < 400) {
-        var result = JSON.parse(r.response);
-        showData(result.videos[0]);
+        var results = JSON.parse(r.response); //'global' breyta
+        showData(results); //sendum gögnin í showData fallið
       }
     }; //gera ráðstafanir fyrir errors?
-
     r.send();
   }
 
-  function showData(data) { //sýnum upplýsingarnar sem við viljum sýna
-    console.log(data);
-  }
-
   function init() {
-    results = document.querySelector('.results'); //result divið
-    button = document.querySelector('.generate'); //búa til takkinn
-    readJSON();
+    content = document.querySelector('.content'); //divið í index.html
+    readJSON(); //lesum videos.json
     //button.addEventListener('click', click);
   }
 
